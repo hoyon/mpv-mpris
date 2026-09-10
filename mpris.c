a6fa73c961ec4878d5430e011204a2d5b2297220
crvs <carvas.f@gmail.com>
2026-09-10 09:39:03 +0200

    Always add suffix to the player name
    In the previous behaviour, if you have two instances of mpv, the first
    one will be named `mpv` and the second `mpv.instanceXXXX`, this will
    mean that if you do.
    
    $ playerctl pause -p mpv
    
    Both instances will be paused, instead of just the first one, whereas
    selecting `mpv.instanceXXXX` will  correctly pause only one of them.

--- mpris.c
+++ mpris.c
@@ -1003,7 +1003,7 @@ static char *generate_random_id(void)
     return id;
 }
 
-static char *build_bus_name(const char *client_name, gboolean unique_suffix)
+static char *build_bus_name(const char *client_name)
 {
     GString *name = g_string_new("org.mpris.MediaPlayer2.mpv");
 
@@ -1012,11 +1012,9 @@ static char *build_bus_name(const char *client_name, gboolean unique_suffix)
         g_string_append_printf(name, ".%s", client_name);
     }
 
-    if (unique_suffix) {
-        char *id = generate_random_id();
-        g_string_append_printf(name, ".instance-%s", id);
-        g_free(id);
-    }
+    char *id = generate_random_id();
+    g_string_append_printf(name, ".instance-%s", id);
+    g_free(id);
 
     return g_string_free(name, FALSE);
 }
@@ -1028,7 +1026,7 @@ static void on_name_lost(GDBusConnection *connection,
     UserData *ud = user_data;
 
     if (connection) {
-        char *name = build_bus_name(ud->client_name, TRUE);
+        char *name = build_bus_name(ud->client_name);
         ud->bus_id = g_bus_own_name(G_BUS_TYPE_SESSION,
                                     name,
                                     G_BUS_NAME_OWNER_FLAGS_NONE,
@@ -1276,7 +1274,7 @@ int mpv_open_cplugin(mpv_handle *mpv)
     mpv_get_property(mpv, "playlist-count", MPV_FORMAT_INT64, &ud.playlist_count);
     mpv_get_property(mpv, "playlist-pos", MPV_FORMAT_INT64, &ud.playlist_pos);
 
-    char *bus_name = build_bus_name(ud.client_name, FALSE);
+    char *bus_name = build_bus_name(ud.client_name);
     g_main_context_push_thread_default(ctx);
     ud.bus_id = g_bus_own_name(G_BUS_TYPE_SESSION,
                                bus_name,
